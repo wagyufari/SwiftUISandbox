b07640bd9526758290a4a3adc57158acce42bb12
Muhammad Ghifari <ghifari9567@gmail.com>
2022-04-05 06:29:48 +0700

    Initial Commit

---
+++ SwiftUIProject/SwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProjectApp.swift
+//  SwiftUIProject
+//
+//  Created by Muhammad Ghifari on 05/04/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
